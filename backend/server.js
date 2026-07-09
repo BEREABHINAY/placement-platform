fb61930632ed61c9b9bcb8156d264d8634af70a5
BEREABHINAY <154392223+BEREABHINAY@users.noreply.github.com>
2026-07-09 16:08:35 +0530

    Update CORS configuration to allow multiple origins

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+        "http://localhost:5173",
+        "https://placementprep-wheat.vercel.app",
+        process.env.CLIENT_URL
+    ],
     credentials: true,
 }));
 app.use(express.json());
